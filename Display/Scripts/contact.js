9585e1087dfebb6a2d12e263b1243ddcae4fde5a
Thomas Thornton <thomas.thornton.jr@gmail.com>
2011-08-24 13:48:15 -0700

    jquery validation working well in contact form

---
+++ Display/Scripts/contact.js
@@ -0,0 +1,24 @@
+﻿jQuery(document).ready(function () {
+    $('.help-inline').hide();
+    jQuery.validator.messages.required = "";
+    jQuery("form").validate({
+        rules: {
+            name: "required",
+            email: "required",
+            subject: "required",
+            message: "required"
+        },
+        highlight: function (element, errorClass, validClass) {
+            $(element).parent().addClass("error").find("span").show();
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).parent().removeClass("error").find("span").hide();
+        },
+        onkeyup: false,
+        submitHandler: function () {
+            alert("in submit handler");
+            element.parent().removeClass("error");
+            alert("submit! use link below to go to the other step");
+        }
+    });
+});
